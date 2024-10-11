aa9539193c3f7c1c4ecf0d2eca23ade7b7adbcb2
Viacheslav-ua <atph@ukr.net>
2024-10-11 11:26:51 +0300

    Created Logo Component

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,15 @@
+export const Logo = () => {
+  return (
+    <div className="w-28 bg-yellow-300 rounded-full border-2 border-sky-500 font-bold flex items-center">
+      <p className="text-sm mb-1 text-sky-800 italic mr-2 pl-3">react</p>
+      <p className=" h-8  flex items-center pr-3">
+        <p className="text-red-600 pb-3">S</p>
+        <p className="text-blue-600 pb-2">K</p>
+        <p className="text-blue-600 pb-1">I</p>
+        <p className="text-green-600">L</p>
+        <p className="text-green-600 pt-1">L</p>
+        <p className="text-red-600 pt-2">S</p>
+      </p>
+    </div>
+  );
+}
--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { Logo } from "app/components/Logo"
+
 export const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Logo />
     </div>
   )
 }
