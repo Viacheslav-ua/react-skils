a9d1e168bf96ef790bab5c47dbcda80a1c5569fa
Viacheslav-ua <atph@ukr.net>
2024-12-15 07:03:43 +0200

    Added Portal component

--- src/shared/ui/Modal.tsx
+++ src/shared/ui/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC, useCallback, useEffect, useRef, useState } from 'react'
 import clsx from 'clsx'
+import { Portal } from './Portal'
 
 interface ModalProps {
   className?: string
@@ -20,7 +21,7 @@ export const Modal: FC<ModalProps> = (props) => {
   const contentClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation()
   }
-  
+
   const closeHandler = useCallback(() => {
     if (onClose) {
       setIsClosing(true)
@@ -36,7 +37,7 @@ export const Modal: FC<ModalProps> = (props) => {
       closeHandler()
     }
   }, [closeHandler])
-  
+
   useEffect(() => {
     if (isOpen) {
       window.addEventListener('keydown', keydownHandler)
@@ -48,25 +49,28 @@ export const Modal: FC<ModalProps> = (props) => {
   }, [isOpen, keydownHandler])
 
   return (
-    <div className={clsx(
-      'fixed top-0 left-0 right-0 bottom-0',
-      isOpen ? 'opacity-1 pointer-events-auto z-10' : 'z-[-1] opacity-0 pointer-events-none',
-      className
-    )}>
+    <Portal>
       <div className={clsx(
-        'top-0 left-0 w-full h-full flex justify-center items-center bg-slate-900/60',
-                   
-      )} onClick={closeHandler}>
-        <div onClick={contentClickHandler}
-          className={clsx(
-            'p-6 rounded-xl bg-slate-200',
-            isOpen && 'animate-zoomIn',
-            isClosing && 'animate-zoomOut',
-          )}
-        >
-          {children}
+        'fixed top-0 left-0 right-0 bottom-0',
+        isOpen ? 'opacity-1 pointer-events-auto z-10' : 'z-[-1] opacity-0 pointer-events-none',
+        className
+      )}>
+        <div className={clsx(
+          'top-0 left-0 w-full h-full flex justify-center items-center bg-slate-900/60',
+
+        )} onClick={closeHandler}>
+          <div onClick={contentClickHandler}
+            className={clsx(
+              'p-6 rounded-xl bg-slate-200',
+              isOpen && 'animate-zoomIn',
+              isClosing && 'animate-zoomOut',
+            )}
+          >
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </Portal>
+
   )
 }
